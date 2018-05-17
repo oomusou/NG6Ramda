d91b11278be14e87f8cf79b47cb5ce5d2000fc95
oomusou <oomusou@gmail.com>
2018-05-17 21:39:11 +0800

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import compose from 'ramda/src/compose';
+import toUpper from 'ramda/src/toUpper';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit(): void {
+    const classyGreeting = (firstName, lastName) =>
+      'The name\'s ' + lastName + ', ' + firstName + ' ' + lastName;
+
+    const yellGreeting = compose(toUpper, classyGreeting);
+    console.log(yellGreeting('James', 'Bond'));
+  }
 }
